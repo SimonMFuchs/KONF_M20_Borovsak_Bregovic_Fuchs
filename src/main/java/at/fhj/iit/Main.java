4f43a9072e851acc927dc4ee970c2788d8a37554
katarina.borovsak <borovsak.katarina@gmail.com>
2021-03-15 21:11:14 +0100

    Made a new classe Wine

--- src/main/java/at/fhj/iit/Main.java
+++ src/main/java/at/fhj/iit/Main.java
@@ -11,10 +11,17 @@ public class Main {
         Liquid tonic = new Liquid("Tonic", 0.150, 0);
         Liquid gin = new Liquid("Gin", 0.04, 40);
 
+        Liquid wein = new Liquid ("Wine", 0.1, 12);
+        Liquid watter = new Liquid ("Watter", 0.1,0);
+
         Drink d = new SimpleDrink("Rotwein",l);
         System.out.println(d);
 
         Drink ginTonic = new MixedDrink("Gin Tonic", gin, tonic);
         System.out.println(ginTonic);
+
+        Drink spritzer = new Wine ("Spritzer", wein, watter);
+        System.out.println(spritzer);
+
     }
 }
--- src/main/java/at/fhj/iit/Wine.java
+++ src/main/java/at/fhj/iit/Wine.java
@@ -1,4 +1,34 @@
 package at.fhj.iit;
 
-public class Wine {
+public class Wine extends Drink {
+
+    protected Liquid w;
+    protected Liquid a;
+
+
+    Wine (String name, Liquid w, Liquid a) {
+        super(name);
+        this.w = w;
+        this.a = a;
+    }
+
+    @Override
+    public double getVolume() {
+        return w.getVolume() + a.getVolume();
+    }
+
+    @Override
+    public double getAlcoholPercent() {
+        return w.getAlcoholPercent();
+    }
+
+    @Override
+    public boolean isAlcoholic() {
+        if (w.getAlcoholPercent() > 0) {
+            return true;
+        }
+        return false;
+    }
 }
+
+
