7b8677e3068220422ca90490d54eefb90ede3206
Simon Fuchs <simon.fuchs@edu.fh-joanneum.at>
2021-03-15 13:38:25 +0100

    Added MixedDrink Class and implemented into Main Class

--- src/main/java/at/fhj/iit/Main.java
+++ src/main/java/at/fhj/iit/Main.java
@@ -7,8 +7,14 @@ public class Main {
         Liquid l = new Liquid("Wein", 0.125, 13);
         System.out.println(l.getName());
         System.out.println(l.getVolume());
+        
+        Liquid tonic = new Liquid("Tonic", 0.150, 0);
+        Liquid gin = new Liquid("Gin", 0.04, 40);
 
         Drink d = new SimpleDrink("Rotwein",l);
         System.out.println(d);
+
+        Drink ginTonic = new MixedDrink("Gin Tonic", gin, tonic);
+        System.out.println(ginTonic);
     }
 }
---
+++ src/main/java/at/fhj/iit/MixedDrink.java
@@ -0,0 +1,64 @@
+package at.fhj.iit;
+
+/**
+ * represents a alcoholic mixed drink
+ */
+public class MixedDrink extends Drink{
+
+    /**
+     * uses two liquids
+     */
+    protected Liquid l1;
+    protected Liquid l2;
+
+    /**
+     * Creates a MixedDrink object with given name and liquids
+     *
+     * @param name name of drink
+     * @param l1 first liquid
+     * @param l2 second liquid to mix with
+     */
+    MixedDrink(String name, Liquid l1, Liquid l2){
+        super(name);
+        this.l1 = l1;
+        this.l2 = l2;
+    }
+
+    /**
+     * Returns volume of the drink
+     *
+     * @return the volume of drink in litre
+     */
+    @Override
+    public double getVolume() {
+        return l1.getVolume() + l2.getVolume();
+    }
+
+    /**
+     * Returns alcohol volume percent of the drink
+     *
+     * @return alcohol volume percent of the whole drink
+     */
+    @Override
+    public double getAlcoholPercent() {
+        return ((l1.getVolume()) * l1.getAlcoholPercent() + (l2.getVolume()) * l2.getAlcoholPercent())/getVolume();
+    }
+
+    /**
+     * Gives information if drink is alcoholic or not
+     *
+     * @return true when alcoholic liquids are present, otherwise false
+     */
+    @Override
+    public boolean isAlcoholic() {
+        if(l1.getAlcoholPercent() > 0 || l2.getAlcoholPercent() > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public String toString(){
+        String alcoholPercent = String.format("%.2f", getAlcoholPercent());
+        return "Mixed Drink called " + name + " with " + alcoholPercent + " percent alcohol by volume";
+    }
+}
